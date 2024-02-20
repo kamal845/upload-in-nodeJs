05b4e95170c1f947619e239dc2b85ffa6e7a32f7
kamal <kk5738547@gmail.com>
2024-02-20 23:52:01 +0530

    newcommit.

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
-const express=require('express')
-const app=express();
-port=3000;
-const connectDB=require("./database/connectDB");
+const express = require("express");
+const app = express();
+const path = require("path");
+const multer=require('multer');
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+app.use(express.urlencoded({ extended: false }));
+port = 4000;
+const connectDB = require("./database/connectDB");
+
+app.get("/", (req, res) => {
+  res.render("index2");
+});
+//for file upload
+app.post("/upload", (req, res) => {
+  res.send("file upload");
+});
+//
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, res, cb) {
+      cb(null, "/uploads");
+    },
+    filename: function (req, res, cb) {
+cb(null,file.fieldname + "-" + Date.now() + '.jpg')
+    }
+  }),
+});
 try {
-    app.listen(port, (req,res) => {
-        connectDB().then(() => {
-            console.log("Serverrrrr is started on port no. 3000");
-        }).catch(error => {
-            console.log("Error connecting to the database:", error);
-        });
-    });
+  app.listen(port, (req, res) => {
+    connectDB()
+      .then(() => {
+        console.log("Server is started on port no. 4000");
+      })
+      .catch((error) => {
+        console.log("Error connectting to the database:", error);
+      });
+  });
 } catch (error) {
-    console.log("Error:", error);
+  console.log("Error:", error);
 }
